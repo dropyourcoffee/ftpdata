73434ca54e333655391ac901d3b8932ff0967df7
Dexter Jung <dropyourcoffee@gmail.com>
2021-06-21 04:12:45 +0900

    hotfix for filter_by + lint

--- setup.py
+++ setup.py
@@ -8,3 +8,13 @@ setup(
     version=__version__,
     install_requires=['paramiko'],
 )
+
+"""
+ Upload Steps
+ 
+  1. Check __version__
+  2. Build whl
+      python setup.py bdist_wheel
+  
+  3. twine upload dist/fitdata-{version}-py3-none-any.whl
+"""
