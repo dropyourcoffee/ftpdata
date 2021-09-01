ca158dbd7254143ffb00d933ba0825d5620822e9
Dexter Jung <dropyourcoffee@gmail.com>
2021-09-01 12:55:51 +0900

    sess.add + Directory module added

--- testbench/test_engine.py
+++ testbench/test_engine.py
@@ -3,32 +3,49 @@ import pytest
 import os
 import rsa
 import re
-from ftpdata import create_engine
+from ftpdata import create_engine, Directory
 from ftpdata.exceptions import DialectValidationError, AuthenticationError, SSHError
 from testbench.MockSFTP import MockSFTP
 
 
 class TestEngine(MockSFTP):
 
-    def test_1_create_engine_sftp(self):
+    def test_01_create_engine_sftp(self):
         engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
         engine()
 
-    def test_2_create_engine_ftps(self):
+    def test_02_create_engine_ftps(self):
         engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
         sess = engine()
-        print([f.name for f in sess.query("/testdata")])
 
-    def test_3_invalid_url(self):
+        class TargetDirectory(Directory):
+            __path__ = '/testdata'
+
+        self.assertEqual([f.name for f in sess.query(TargetDirectory)],
+                         ['sample.csv']
+                         )
+
+    def test_03_path_not_defined_in_target_directory(self):
+
+        engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
+        sess = engine()
+
+        class TargetDirectory(Directory):
+            pass
+
+        with pytest.raises(AttributeError):
+            [f.name for f in sess.query(TargetDirectory).filter_by(re.compile('^sample'))]
+
+    def test_04_invalid_url(self):
         with pytest.raises(DialectValidationError):
             create_engine("sftp:///s")
 
-    def test_4_auth_info_not_given(self):
+    def test_05_auth_info_not_given(self):
         with pytest.raises(AuthenticationError):
             engine = create_engine(self.mock_sftp_config.url)
             engine()
 
-    def test_5_auth_passwd_not_given(self):
+    def test_06_auth_passwd_not_given(self):
         with pytest.raises(AuthenticationError):
             engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username)
             engine()
@@ -36,8 +53,7 @@ class TestEngine(MockSFTP):
         with pytest.raises(SSHError):
             create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=1)
 
-
-    def test_6_wrong_key_passed(self):
+    def test_07_wrong_key_passed(self):
         with pytest.raises(AuthenticationError):
             _, fake_key = rsa.newkeys(2048)
             fake_keyfile = "fake_key.pem"
@@ -49,18 +65,67 @@ class TestEngine(MockSFTP):
             engine()
         os.remove(fake_keyfile)
 
-    def test_7_spec_filter_by_str(self):
+    def test_08_spec_filter_by_str(self):
         engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
         sess = engine()
-        files = [f.name for f in sess.query("/testdata").filter_by("sample")]
-        self.assertEqual(files, ['sample.csv'])
 
-    def test_8_spec_filter_by_regex(self):
+        class TargetDirectory:
+            __path__ = '/testdata'
+
+        files = [f.name for f in sess.query(TargetDirectory).filter_by("sample")]
+
+        self.assertEqual(files,
+                         ['sample.csv']
+                         )
+
+    def test_09_spec_filter_by_regex(self):
         engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
         sess = engine()
-        files = [f.name for f in sess.query("/testdata").filter_by(re.compile('^sample'))]
-        self.assertEqual(files, ['sample.csv'])
 
+        class TargetDirectory(Directory):
+            __path__ = '/testdata'
+
+        files = [f.name for f in sess.query(TargetDirectory).filter_by(re.compile('^sample'))]
+
+        self.assertEqual(files,
+                         ['sample.csv']
+                         )
+
+
+    def test_10_inline_fn(self):
+        engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
+        sess = engine()
+
+        class TargetDirectory(Directory):
+            __path__ = '/testdata'
+
+            @staticmethod
+            def fn(fstat):
+                fstat.filesize_s = f"{fstat.st_size} bytes"
+                return fstat
+
+        files = [f.filesize_s for f in sess.query(TargetDirectory).filter_by("sample")]
+
+        self.assertEqual(files,
+                         ['41 bytes']
+                         )
+
+    def test_11_add_file(self):
+        engine = create_engine(self.mock_sftp_config.url, username=self.mock_sftp_config.username, pkey=self.mock_sftp_config.keyfile)
+
+        sess = engine()
+
+        class TargetDirectory(Directory):
+            __path__ = '/testdata'
+
+        new_file = TargetDirectory('./newfile')
+        sess.add(new_file)
+
+        files = [f.name for f in sess.query(TargetDirectory).filter_by("new")]
+
+        self.assertEqual(files,
+                         ['newfile']
+                         )
 
 if __name__ == "__main__":
     unittest.main()
